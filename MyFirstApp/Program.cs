c78e46758d9eb157f56946392d491bca3808f671
Fjodormelehhov <fjodor.melehhov@tktk.ee>
2023-09-09 18:31:58 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name: ");// output
+//stringe - sõne
+string username;
+
+username = Console.ReadLine();// input
+
+Console.WriteLine("Hello" + ", " + username + "!");// output
+
+// string interpolation
+Console.WriteLine($"Hello, {username}!"); // output
